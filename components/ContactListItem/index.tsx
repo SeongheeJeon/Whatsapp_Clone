a08f6fcbef757bb83ebecc10ff895b8c3768e913
DESKTOP-76V6K1K\se5ng <c9604100@gmail.com>
2021-10-09 11:09:16 +0900

    create chat room at amplify

--- components/ContactListItem/index.tsx
+++ components/ContactListItem/index.tsx
@@ -5,6 +5,12 @@ import { User } from "../../types";
 import styles from "./styles";
 import { useNavigation } from "@react-navigation/native";
 
+import { API, Auth, graphqlOperation } from "aws-amplify";
+import {
+  createChatRoom,
+  createChatRoomUser,
+} from "../../src/graphql/mutations";
+
 export type ContactListItemProps = {
   user: User;
 };
@@ -14,7 +20,49 @@ const ContactListItem = (props: ContactListItemProps) => {
 
   const navigation = useNavigation();
 
-  const onClick = () => {
+  const onClick = async () => {
+    try {
+      // 1. Create a new Chat Room
+      const newChatRoomData = await API.graphql(
+        graphqlOperation(createChatRoom, { input: {} })
+      );
+
+      if (!newChatRoomData.data) {
+        console.log("Failed to create a chat room");
+        return;
+      }
+
+      const newChatRoom = newChatRoomData.data.createChatRoom;
+
+      // 2. Add 'user' to the Chat Room
+      await API.graphql(
+        graphqlOperation(createChatRoomUser, {
+          input: {
+            userID: user.id,
+            chatRoomID: newChatRoom.id,
+          },
+        })
+      );
+
+      // 3. Add authenticated user to thet Chat Room
+      const userInfo = await Auth.currentAuthenticatedUser();
+      await API.graphql(
+        graphqlOperation(createChatRoomUser, {
+          input: {
+            userID: userInfo.attributes.sub,
+            chatRoomID: newChatRoom.id,
+          },
+        })
+      );
+
+      navigation.navigate("ChatRoom", {
+        id: newChatRoom.id,
+        name: "Hardcoded name",
+        imageUri: user.imageUri,
+      });
+    } catch (e) {
+      console.log(e);
+    }
     // navigate to chat room with this user
   };
 
